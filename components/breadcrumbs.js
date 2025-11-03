4871d690fc336f83543e988648b81315bf4d3fe2
s3mbx3 <staffshow228@gmail.com>
2025-11-04 01:49:01 +0300

    Update breadcrumbs.js
    functions breadcrumbs

--- components/breadcrumbs.js
+++ components/breadcrumbs.js
@@ -1,1 +1,38 @@
-console.log('Breadcrumbs added');
+class Breadcrumbs {
+    constructor() {
+        this.container = null;
+    }
+    
+    generate(path) {
+        const parts = path.split('#');
+        let breadcrumbs = '<div class="breadcrumbs">';
+        let currentPath = '';
+        
+        parts.forEach((part, index) => {
+            if (index > 0) currentPath += '#';
+            currentPath += part;
+            
+            const isLast = index === parts.length - 1;
+            const name = this.getDisplayName(part);
+            
+            if (isLast) {
+                breadcrumbs += `<span class="breadcrumb-current">${name}</span>`;
+            } else {
+                breadcrumbs += `<a href="#${currentPath}" class="breadcrumb-link">${name}</a> › `;
+            }
+        });
+        
+        breadcrumbs += '</div>';
+        return breadcrumbs;
+    }
+    
+    getDisplayName(part) {
+        const names = {
+            'users': '👥 Пользователи',
+            'todos': '✅ Задачи',
+            'posts': '📝 Посты', 
+            'comments': '💬 Комментарии'
+        };
+        return names[part] || part;
+    }
+}
